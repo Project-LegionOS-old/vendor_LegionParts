fce4293bae42c226214ec97a51fbc34107c17c4b
Aston-Martinn <advaithbhat9@gmail.com>
2021-08-03 02:18:10 +0530

    themes: Introduce Custom Statusbar Signal & Wi-Fi icons [3/3]
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- parts.mk
+++ parts.mk
@@ -112,3 +112,25 @@ PRODUCT_PACKAGES += \
     GesturalNavigationRadiusLow \
     GesturalNavigationRadiusVeryLow \
     GesturalNavigationRadiusHidden
+
+# Statusbar Icons
+PRODUCT_PACKAGES += \
+    StrokeSignalOverlay \
+    SneakySignalOverlay \
+    XperiaSignalOverlay \
+    ZigZagSignalOverlay \
+    WavySignalOverlay \
+    RoundSignalOverlay \
+    InsideSignalOverlay \
+    BarsSignalOverlay
+
+# Wi-Fi Icons
+PRODUCT_PACKAGES += \
+    StrokeWiFiOverlay \
+    SneakyWiFiOverlay \
+    XperiaWiFiOverlay \
+    ZigZagWiFiOverlay \
+    WavyWiFiOverlay \
+    RoundWiFiOverlay \
+    InsideWiFiOverlay \
+    BarsWiFiOverlay
